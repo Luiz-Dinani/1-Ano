ff9090615bdf565c9b282c6fce0c7f1f998d9450
Luiz-Dinani <79462872+Luiz-Dinani@users.noreply.github.com>
2021-11-12 11:40:10 -0300

    Melhorando o Exercício Vendedor (toString para a classe filha)
    Commit changes

---
+++ Linguagem de Programação/projeto11-heranca/src/main/java/exercicio/vendedor/comissao/App.java
@@ -0,0 +1,24 @@
+package exercicio.vendedor.comissao;
+
+/*
+ * @author aluno
+ */
+public class App {    
+    /**
+     * @param args the command line arguments
+     */    
+    public static void main(String[] args) {
+        VendedorComissao vend1 = new VendedorComissao("01", "Jorge", 100000.00, 10.00);
+        VendedorComissaoMaisSalario vend2 = new VendedorComissaoMaisSalario("02", "Glaciete", 100000.00, 10.00, 1000.00);
+        Departamento dep = new Departamento();
+
+        dep.adicionarVendedor(vend1);
+        dep.adicionarVendedor(vend2);
+
+        System.out.println(vend1.calcularSalario());
+        System.out.println(vend2.calcularSalario());
+        System.out.println("=".repeat(15));
+        System.out.println(String.format("Total pago em Salário: %.2f", dep.calcularTotalSalario()));
+        dep.exibeTodos();
+    }   
+}
---
+++ Linguagem de Programação/projeto11-heranca/src/main/java/exercicio/vendedor/comissao/VendedorComissaoMaisSalario.java
@@ -0,0 +1,32 @@
+package exercicio.vendedor.comissao;
+
+/**
+ *
+ * @author aluno
+ */
+public class VendedorComissaoMaisSalario extends VendedorComissao{
+
+    private Double salarioFixo;
+
+    public VendedorComissaoMaisSalario(String codigo, String nome, Double vendas, Double taxa, Double salarioFixo) {
+        super(codigo, nome, vendas, taxa);
+        this.salarioFixo = salarioFixo;
+    }
+        
+    @Override
+    public Double calcularSalario() {
+        return super.calcularSalario()+salarioFixo; //To change body of generated methods, choose Tools | Templates.
+    }    
+
+
+    @Override
+    public String toString() {
+        return "{" +
+        " codigo='" + super.getCodigo() + "'" +
+        ", nome='" + super.getNome() + "'" +
+        ", vendas='" + super.getVendas() + "'" +
+        ", taxa='" + super.getTaxa() + "'" +
+            " salarioFixo='" + salarioFixo + "'" +
+            "}";
+    }
+}
